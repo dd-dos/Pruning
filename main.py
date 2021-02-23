d09fe057e32a065a824104e8549dde011fffa7a9
dd-dos <dungphamdang99@gmail.com>
2021-02-23 11:08:35 +0700

    fast commit

---
+++ main.py
@@ -0,0 +1,90 @@
+from Pytorch_MobileNetV2 import MobileNetV2
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import numpy as np
+import os
+
+BEST_ACC = 0
+
+def train_pytorch(checkpoint):
+    transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                            shuffle=True, num_workers=2)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net = MobileNetV2(n_class=10, input_size=32).to(device)
+
+    for epoch in range(10):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = inputs.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+        test_pytorch(net, checkpoint)
+        
+def test_pytorch(net, checkpoint):
+    global BEST_ACC
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                        download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                            shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    current_acc = np.sum(class_correct) / np.sum(class_total)
+    if current_acc > BEST_ACC:
+        BEST_ACC = current_acc
+        torch.save(net, os.path.join(checkpoint, "best.pth"))
+    else:
+        torch.save(net, os.path.join(checkpoint, "last.pth"))
+
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
+        print("Overall accuracy: {}" current_acc)
