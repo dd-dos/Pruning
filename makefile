00138e5b7f12e7d501292b12b8fda2031a1a5240
dd-dos <dungphamdang99@gmail.com>
2021-02-24 12:42:39 +0700

    fast commit

--- makefile
+++
@@ -1,8 +0,0 @@
-.PHONY: all git_process
-
-all: git_process
-
-git_process:
-	@git add .
-	@git commit -m "fast commit"
-	@git push
--- scripts/train.sh
+++
@@ -1,2 +0,0 @@
-python main.py --cp /content/drive/MyDrive/training/Pruning \
-               --pretrained /content/drive/MyDrive/training/Pruning/best.pth 
