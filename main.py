dff0b6ef9e21dbc5ea5d850a2849fdcc569e22cf
dd-dos <dungphamdang99@gmail.com>
2021-02-25 21:20:56 +0700

    fast commit

--- main.py
+++ main.py
@@ -118,8 +118,8 @@ def prune_global_unstructured(net_creator, imagenet_path, batch_size):
             file.write("method: glob_unstr - prune amount: {:.0%} - accuracy: {:.2f} \n".format(amount, result))
 
 def prune_l1_unstructured(net_creator, imagenet_path, batch_size):
-    for i in range(4,5):
-        for idx in range(2,18):
+    for i in range(4,10):
+        for idx in range(2,3):
             net = net_creator()
             module = net.features[idx].conv
             amount = i/10
